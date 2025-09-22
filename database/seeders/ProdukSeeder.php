ad2d36c2e086ff4e27f64f41d3c7b23836dab74c
adam adifam <adamabdi.al.a@gmail.com>
2025-09-23 01:36:06 +0700

    update

--- database/seeders/ProdukSeeder.php
+++ database/seeders/ProdukSeeder.php
@@ -15,10 +15,9 @@ class ProdukSeeder extends Seeder
     public function run(): void
     {
         $produks = [
-            // Rempah-Rempah (Kategori ID: 1)
             [
-                'kode_produk' => 'RMP001',
-                'nama_produk' => 'Cengkeh Kering Grade A',
+                'kode_produk' => 'KMD001',
+                'nama_produk' => 'Cengkeh',
                 'kategori_id' => 1,
                 'satuan_id' => 1, // Kg
                 'harga_jual' => 145000,
@@ -28,41 +27,74 @@ class ProdukSeeder extends Seeder
                 'updated_at' => now(),
             ],
             [
-                'kode_produk' => 'RMP002',
-                'nama_produk' => 'Pala Utuh Premium',
+                'kode_produk' => 'KMD002',
+                'nama_produk' => 'Kapol',
                 'kategori_id' => 1,
                 'satuan_id' => 1, // Kg
-                'harga_jual' => 220000,
+                'harga_jual' => 105000,
                 'stok' => 175,
                 'stok_minimal' => 25,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'kode_produk' => 'RMP003',
-                'nama_produk' => 'Kapol (Bunga Pala)',
+                'kode_produk' => 'KMD003',
+                'nama_produk' => 'Kopi',
                 'kategori_id' => 1,
                 'satuan_id' => 1, // Kg
-                'harga_jual' => 105000,
+                'harga_jual' => 95000,
+                'stok' => 350,
+                'stok_minimal' => 40,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_produk' => 'KMD004',
+                'nama_produk' => 'Coklat',
+                'kategori_id' => 1,
+                'satuan_id' => 1, // Kg
+                'harga_jual' => 45000,
+                'stok' => 720,
+                'stok_minimal' => 80,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_produk' => 'KMD005',
+                'nama_produk' => 'Rinu',
+                'kategori_id' => 1,
+                'satuan_id' => 1, // Kg
+                'harga_jual' => 85000,
                 'stok' => 120,
                 'stok_minimal' => 20,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'kode_produk' => 'RMP004',
-                'nama_produk' => 'Lada Hitam Lampung',
+                'kode_produk' => 'KMD006',
+                'nama_produk' => 'Pakang',
                 'kategori_id' => 1,
                 'satuan_id' => 1, // Kg
-                'harga_jual' => 115000,
-                'stok' => 280,
-                'stok_minimal' => 30,
+                'harga_jual' => 65000,
+                'stok' => 95,
+                'stok_minimal' => 15,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'kode_produk' => 'RMP005',
-                'nama_produk' => 'Lada Putih Bangka',
+                'kode_produk' => 'KMD007',
+                'nama_produk' => 'Jagung',
+                'kategori_id' => 1,
+                'satuan_id' => 1, // Kg
+                'harga_jual' => 12000,
+                'stok' => 800,
+                'stok_minimal' => 100,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_produk' => 'KMD008',
+                'nama_produk' => 'Lada Pt',
                 'kategori_id' => 1,
                 'satuan_id' => 1, // Kg
                 'harga_jual' => 135000,
@@ -72,55 +104,20 @@ class ProdukSeeder extends Seeder
                 'updated_at' => now(),
             ],
             [
-                'kode_produk' => 'RMP006',
-                'nama_produk' => 'Kayu Manis Cassia',
+                'kode_produk' => 'KMD009',
+                'nama_produk' => 'Lada Hitam',
                 'kategori_id' => 1,
                 'satuan_id' => 1, // Kg
-                'harga_jual' => 58000,
-                'stok' => 8, // Stok menipis
-                'stok_minimal' => 10,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-
-            // Hasil Perkebunan (Kategori ID: 2)
-            [
-                'kode_produk' => 'PKB001',
-                'nama_produk' => 'Kopi Arabika Gayo',
-                'kategori_id' => 2,
-                'satuan_id' => 1, // Kg
-                'harga_jual' => 95000,
-                'stok' => 350,
-                'stok_minimal' => 40,
+                'harga_jual' => 115000,
+                'stok' => 280,
+                'stok_minimal' => 30,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'kode_produk' => 'PKB002',
-                'nama_produk' => 'Kopi Robusta Lampung',
-                'kategori_id' => 2,
-                'satuan_id' => 1, // Kg
-                'harga_jual' => 60000,
-                'stok' => 520,
-                'stok_minimal' => 50,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'kode_produk' => 'PKB003',
-                'nama_produk' => 'Kakao Fermentasi',
-                'kategori_id' => 2,
-                'satuan_id' => 1, // Kg
-                'harga_jual' => 45000,
-                'stok' => 720,
-                'stok_minimal' => 80,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'kode_produk' => 'PKB004',
-                'nama_produk' => 'Vanili Planifolia',
-                'kategori_id' => 2,
+                'kode_produk' => 'KMD010',
+                'nama_produk' => 'Vaneli',
+                'kategori_id' => 1,
                 'satuan_id' => 1, // Kg
                 'harga_jual' => 3200000,
                 'stok' => 20,
@@ -129,96 +126,134 @@ class ProdukSeeder extends Seeder
                 'updated_at' => now(),
             ],
             [
-                'kode_produk' => 'PKB005',
-                'nama_produk' => 'Kopi Luwak Premium',
-                'kategori_id' => 2,
+                'kode_produk' => 'KMD011',
+                'nama_produk' => 'Pala',
+                'kategori_id' => 1,
                 'satuan_id' => 1, // Kg
-                'harga_jual' => 1200000,
-                'stok' => 0, // Stok habis
-                'stok_minimal' => 2,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-
-            // Biji-Bijian (Kategori ID: 3)
-            [
-                'kode_produk' => 'BJI001',
-                'nama_produk' => 'Kemiri Kupas',
-                'kategori_id' => 3,
-                'satuan_id' => 1, // Kg
-                'harga_jual' => 32000,
-                'stok' => 210,
-                'stok_minimal' => 30,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'kode_produk' => 'BJI002',
-                'nama_produk' => 'Kacang Tanah Kupas',
-                'kategori_id' => 3,
-                'satuan_id' => 1, // Kg
-                'harga_jual' => 24000,
-                'stok' => 420,
-                'stok_minimal' => 50,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-
-            // Umbi-Umbian (Kategori ID: 4)
-            [
-                'kode_produk' => 'UMB001',
-                'nama_produk' => 'Jahe Gajah Kering',
-                'kategori_id' => 4,
-                'satuan_id' => 1, // Kg
-                'harga_jual' => 35000,
-                'stok' => 180,
+                'harga_jual' => 220000,
+                'stok' => 175,
                 'stok_minimal' => 25,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'kode_produk' => 'UMB002',
-                'nama_produk' => 'Kunyit Bubuk',
-                'kategori_id' => 4,
+                'kode_produk' => 'KMD012',
+                'nama_produk' => 'Minyak Pala',
+                'kategori_id' => 1,
                 'satuan_id' => 1, // Kg
-                'harga_jual' => 28000,
-                'stok' => 125,
-                'stok_minimal' => 20,
+                'harga_jual' => 450000,
+                'stok' => 45,
+                'stok_minimal' => 10,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'kode_produk' => 'UMB003',
-                'nama_produk' => 'Lengkuas Kering',
-                'kategori_id' => 4,
+                'kode_produk' => 'KMD013',
+                'nama_produk' => 'Minyak Clo',
+                'kategori_id' => 1,
                 'satuan_id' => 1, // Kg
-                'harga_jual' => 20000,
-                'stok' => 85,
+                'harga_jual' => 380000,
+                'stok' => 60,
+                'stok_minimal' => 12,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_produk' => 'KMD014',
+                'nama_produk' => 'Abu Cengkeh',
+                'kategori_id' => 1,
+                'satuan_id' => 1, // Kg
+                'harga_jual' => 25000,
+                'stok' => 200,
+                'stok_minimal' => 30,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_produk' => 'KMD015',
+                'nama_produk' => 'Jambe',
+                'kategori_id' => 1,
+                'satuan_id' => 1, // Kg
+                'harga_jual' => 18000,
+                'stok' => 150,
+                'stok_minimal' => 25,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_produk' => 'KMD016',
+                'nama_produk' => 'Daun Ck',
+                'kategori_id' => 1,
+                'satuan_id' => 1, // Kg
+                'harga_jual' => 35000,
+                'stok' => 80,
                 'stok_minimal' => 15,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
-
-            // Buah Kering (Kategori ID: 5)
             [
-                'kode_produk' => 'BKR001',
-                'nama_produk' => 'Kelapa Parut Kering',
-                'kategori_id' => 5,
+                'kode_produk' => 'KMD017',
+                'nama_produk' => 'Buah Laja',
+                'kategori_id' => 1,
                 'satuan_id' => 1, // Kg
-                'harga_jual' => 16000,
-                'stok' => 250,
-                'stok_minimal' => 35,
+                'harga_jual' => 75000,
+                'stok' => 110,
+                'stok_minimal' => 20,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'kode_produk' => 'BKR002',
-                'nama_produk' => 'Asam Jawa Kering',
-                'kategori_id' => 5,
+                'kode_produk' => 'KMD018',
+                'nama_produk' => 'BM',
+                'kategori_id' => 1,
                 'satuan_id' => 1, // Kg
-                'harga_jual' => 12000,
-                'stok' => 150,
-                'stok_minimal' => 20,
+                'harga_jual' => 95000,
+                'stok' => 90,
+                'stok_minimal' => 15,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_produk' => 'KMD019',
+                'nama_produk' => 'Sempra',
+                'kategori_id' => 1,
+                'satuan_id' => 1, // Kg
+                'harga_jual' => 125000,
+                'stok' => 140,
+                'stok_minimal' => 25,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_produk' => 'KMD020',
+                'nama_produk' => 'Kayu Manis',
+                'kategori_id' => 1,
+                'satuan_id' => 1, // Kg
+                'harga_jual' => 58000,
+                'stok' => 8,
+                'stok_minimal' => 10,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_produk' => 'KMD021',
+                'nama_produk' => 'Sempra Broken',
+                'kategori_id' => 1,
+                'satuan_id' => 1, // Kg
+                'harga_jual' => 95000,
+                'stok' => 75,
+                'stok_minimal' => 15,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_produk' => 'KMD022',
+                'nama_produk' => 'Cengkeh Hutan',
+                'kategori_id' => 1,
+                'satuan_id' => 1, // Kg
+                'harga_jual' => 120000,
+                'stok' => 65,
+                'stok_minimal' => 15,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
